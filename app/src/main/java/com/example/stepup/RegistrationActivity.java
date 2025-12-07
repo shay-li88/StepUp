8ce4be713d101d774db85ba7f017909f353c85e7
shay-li88 <s75315@haklaiph.org>
2025-12-07 10:22:49 +0200

    Hello

--- app/src/main/java/com/example/stepup/RegistrationActivity.java
+++ app/src/main/java/com/example/stepup/RegistrationActivity.java
@@ -3,6 +3,8 @@ package com.example.stepup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -39,6 +41,14 @@ public class RegistrationActivity extends AppCompatActivity {
                         }
                     }
                 });
+        Button registerButton = findViewById(R.id.btn_register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerButtonClick();
+            }
+        });
+
     }
 
     @Override
