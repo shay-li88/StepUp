eff391c2d083bd18bdb9e964ca8ccc9262b6efc2
shay-li88 <s75315@haklaiph.org>
2026-02-01 17:54:04 +0200

    hiiii

---
+++ app/src/main/java/com/example/stepup/CardioActivity.java
@@ -0,0 +1,21 @@
+package com.example.stepup;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
+
+
+
+public class CardioActivity extends AppCompatActivity {
+
+
+}
